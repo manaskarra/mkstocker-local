73eab92aefb96da3b8462113f563863683ce5e6b
Manas <manas.karra@gmail.com>
2025-03-06 09:52:34 +0400

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/manaskarra/Desktop/stock-portfolio-app/venv"
+set -gx VIRTUAL_ENV "/Users/manaskarra/Desktop/stock-portfolio-app/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
